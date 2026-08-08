cd84ce8dec730a8cf2a265b6c2b22b6c04461e33
Caio Venancio <caiovenancio06@gmail.com>
2026-08-08 20:16:01 -0300

    first commit

---
+++ backend/app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.routes.challenge_routes import router as challenge_router
+from app.database.connection import engine
+from app.models import models
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="GeoQuest API")
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/status")
+def check_api():
+    return {"status": "ok", "message": "GeoQuest API is running!"}
+
+app.include_router(challenge_router, prefix="/api")
