b944b4366dc077c5589086fbf23cf0ae1a8df010
Caio Venancio <caiovenancio06@gmail.com>
2026-08-09 16:44:44 -0300

    feat: adicionar backend em FastAPI

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
 
 from app.routes.challenge_routes import router as challenge_router
 from app.database.connection import engine
